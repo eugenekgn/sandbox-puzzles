fdd3e72977252461d2cf663bf1745129450dfa11
Eugene Kagan <eugenekgn@gmail.com>
2019-10-15 10:03:41 -0400

    moar questrions

--- removeSmallest.js
+++ removeSmallest.js
@@ -6,12 +6,12 @@ const removeSmallest = numbers => {};
 
 describe("Remove smallest number from an array", () => {
   it("Example 1", () => {
-    assert.equal(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+    assert.deepEqual(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
   });
   it("Example 2", () => {
-    assert.equal(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
+    assert.deepEqual(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
   });
   it("Example 3", () => {
-    assert.equal(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
+    assert.deepEqual(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
   });
 });
