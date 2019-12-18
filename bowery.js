2e14d3f668494437ced59772387db442aa3853e8
Eugene Kagan <eugene@Eugenes-MacBook-Pro.local>
2019-12-18 15:07:18 -0500

    trying something new .... scary

---
+++ bowery.js
@@ -0,0 +1,9 @@
+const bowery = [
+  { id: "john/co-founder", parent: "null" },
+  { id: "cesar/co-founder", parent: "null" },
+  { id: "noah/co-founder", parent: "null" },
+  { id: "eugene", parent: "cesar" },
+  { id: "richard", parent: "cesar" },
+  { id: "j3", parent: "richard" },
+  { id: "madeline", parent: "eugene" }
+];
